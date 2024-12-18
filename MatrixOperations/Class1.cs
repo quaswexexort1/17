a350c18281fcfb555f8a8968da9a6b305306f79c
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-12-18 19:02:11 +0200

    Добавьте файлы проекта.

---
+++ MatrixOperations/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace MatrixOperations
+{
+    public static class MatrixHelper
+    {
+        public static void SwapMinMax(int[,] matrix)
+        {
+            if (matrix == null || matrix.Length == 0) return; 
+
+            int minVal = matrix[0, 0];
+            int maxVal = matrix[0, 0];
+            int minRow = 0;
+            int minCol = 0;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < minVal)
+                    {
+                        minVal = matrix[i, j];
+                        minRow = i;
+                        minCol = j;
+                    }
+                    if (matrix[i, j] > maxVal)
+                    {
+                        maxVal = matrix[i, j];
+                        maxRow = i;
+                        maxCol = j;
+                    }
+                }
+            }
+
+            int temp = matrix[minRow, minCol];
+            matrix[minRow, minCol] = matrix[maxRow, maxCol];
+            matrix[maxRow, maxCol] = temp;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using MatrixOperations;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] matrix =
+        {
+            {1, 5, 3},
+            {4, 2, 6},
+            {7, 9, 8}
+        };
+
+        Console.WriteLine("оригинальная матрица:");
+        PrintMatrix(matrix); 
+
+        MatrixHelper.SwapMinMax(matrix);
+
+        Console.WriteLine("\nматрица после:");
+        PrintMatrix(matrix); 
+
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
